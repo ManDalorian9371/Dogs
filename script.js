3278c1ba7d06485c8d7f302f6e465e9f959026bd
mostafa rafati <mostafarafati888@gmail.com>
2022-04-13 00:10:25 +0430

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let opened = document.getElementById('open');
+let closed = document.getElementById('closed');
+let xMark = document.getElementById('xMark');
+let container = document.querySelector('.container');
+opened.addEventListener('click', () => container.classList.add('show-footer'));
+closed.addEventListener('click', () => container.classList.remove('show-footer'));
+xMark.addEventListener('click', () => container.classList.remove('show-footer'));
